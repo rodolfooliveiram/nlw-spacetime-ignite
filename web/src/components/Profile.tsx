c48b9cd0f6dbd84580f652ace27df3a897f8ea41
Rodolfo <rodolfoalvesmdo@gmail.com>
2023-05-21 18:39:40 -0300

    Definição do layout das rotas, implementação de rota de logout, crianção da página de cadastro de memórias e implementando um middleware de autenticação para acesso às páginas da aplicação

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -1,5 +1,6 @@
 import { getUser } from "@/lib/auth";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Profile() {
   const { name, avatarUrl } = getUser();
@@ -18,7 +19,10 @@ export default function Profile() {
 
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
-        <a href="" className="block text-red-400 hover:text-red-300">
+        <a
+          href="/api/auth/logout"
+          className="block text-red-400 hover:text-red-300"
+        >
           Sair
         </a>
       </p>
